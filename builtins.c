3128dd9d7c5927cc2fdcd40fe3d3ff4688c20dab
Dominique Maessen <dmaessen@f1r2s14.codam.nl>
2023-06-14 14:20:08 +0200

    worked on builtins

--- builtins.c
+++ builtins.c
@@ -6,7 +6,7 @@
 /*   By: dmaessen <dmaessen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 17:07:22 by dmaessen          #+#    #+#             */
-/*   Updated: 2023/06/13 18:00:29 by dmaessen         ###   ########.fr       */
+/*   Updated: 2023/06/14 13:02:52 by dmaessen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,17 @@ file for the buildins:
     ◦ exit with no options
 */
 
-void builtin_echo()
+void	builtin_echo(char **argv) // or command (check struct naming)
 {
 	if (ft_strncmp(argv[1], "echo -n", 7) == 0)
-		// printf/write without \n at the end
+		printf("%s", argv[2]); // without \n at the end
 	else
-		// printf/write the given sentence
+		printf("%s\n", argv[2]); // or whatever
 	// for both handle the $ or not ?? think so
 	// what do we return? exit code?
 }
 
-void builtin_cd()
+void	builtin_cd()
 {
 	// Q: whats a relative or absolute path?
 }
@@ -58,10 +58,26 @@ void	builtin_export()
 	// do we support the = something ??
 }
 
-void builtins(char **envp) // still incomplete (envp needed)
+void	builtin_unset() // unset values and attributes of variables and functions
+{
+	// via envp (PATH and SHELL)??
+	// Unsetting a variable or function that was not previously set shall not be considered an error and does not cause the shell to abort.
+}
+
+void	builtin_env()
+{
+	
+}
+
+void	builtin_exit()
+{
+	
+}
+
+void	builtins(char **argv, char **envp) // still incomplete (envp needed)
 {
 	if (ft_strncmp(argv[1], "echo", 4) == 0) // change the argv[1] part (depending on struct)
-		builtin_echo();
+		builtin_echo(argv);
 	else if (ft_strncmp(argv[1], "cd", 2) == 0) // change the argv[1] part (depending on struct)
 		builtin_cd();
 	else if (ft_strncmp(argv[1], "pwd", 3) == 0) // change the argv[1] part (depending on struct)
@@ -69,9 +85,10 @@ void builtins(char **envp) // still incomplete (envp needed)
 	else if (ft_strncmp(argv[1], "export", 6) == 0) // change the argv[1] part (depending on struct)
 		builtin_export();
 	else if (ft_strncmp(argv[1], "unset", 5) == 0) // change the argv[1] part (depending on struct)
-		//
+		builtin_unset();
 	else if (ft_strncmp(argv[1], "env", 3) == 0) // change the argv[1] part (depending on struct)
-		//
+		builtin_env();
 	else if (ft_strncmp(argv[1], "exit", 4) == 0) // change the argv[1] part (depending on struct)
+		builtin_exit();
 		// can't we just use the "real exit" function here, and leave the shell?
 }
