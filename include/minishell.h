bc38315d3be36932ebec50c367f8ebe7d7485cdb
Laura <lpraca-l@student.codam.nl>
2023-06-19 14:08:54 +0200

    updated struct

--- include/minishell.h
+++ include/minishell.h
@@ -7,25 +7,42 @@
 
 #include <stdbool.h>
 
+/**
+ * list of environment variables
+ * @name given name of the variable
+ * @content content of the variable
+ * @next next variable
+ */
 typedef struct t_env_args
 {
     char *name;
     char *content;
-
+    struct t_env_args *next;
 }s_env_args;
 
+/**
+ * list with all tolkens
+ * @str
+ * @in_file boolean in case need read from a file
+ * @out_file boolean in case need to write on a file
+ * @next next item on the list
+ */
 typedef struct t_tolkens
 {
     char *str;
     bool in_file;
     bool out_file;
+    struct t_tolkens *next;
 }s_tolkens;
 
 /**
- * @amount_of_args
- * @tolkens
- * @env_arg
- * @command_line
+ * Main struct with all the information that is needed to run
+ * the commands given to minishell
+ * @amount_of_tolkens how many item on the tolken list and 2D
+ * array
+ * @tolkens tolkens XD
+ * @env_arg list of environment variables
+ * @command_line original command line
  */
 typedef struct t_data
 {
