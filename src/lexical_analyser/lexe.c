32922dfbac4d0ce1ad88944c316a6c675984dd0e
Laura <lpraca-l@student.codam.nl>
2023-06-28 12:49:40 +0200

    shit

--- src/lexical_analyser/lexe.c
+++ src/lexical_analyser/lexe.c
@@ -1,6 +1,23 @@
 #include "../../include/minishell.h"
 #include "../../include/tolken_list_actions.h"
 #include "../../Lib42/include/libft.h"
+void if_pipe(t_data *mini_data, int start, int end)
+{
+    if (mini_data->command_line[end+1] != ' ')
+    {
+        char *str;
+
+        add_token(&mini_data->tokens_list, "|");
+        end++;
+        str = ft_substr(mini_data->command_line, start, end-start);
+        add_token(&mini_data->tokens_list, str);
+    }
+}
+
+void if_iqual(t_data *minidata, int start, int end)
+{
+
+}
 
 void break_cmd(t_data *mini_data)
 {
@@ -12,11 +29,23 @@ void break_cmd(t_data *mini_data)
     end = 0;
     while (mini_data->command_line[end] != '\0')
     {
-        if(mini_data->command_line[end] == ' ')
+        if(ft_strchr(" |=", mini_data->command_line[end]) != 0 )
         {
             str = ft_substr(mini_data->command_line, start, end-start);
             add_token(&mini_data->tokens_list, str);
-            start = end+1;
+            if (mini_data->command_line[end] == '|')
+            {
+                add_token(&mini_data->tokens_list, "|");
+                end++;
+            }
+            if (mini_data->command_line[end] == '=')
+            {
+                add_token(&mini_data->tokens_list, "=");
+                end++;
+            }
+            while(mini_data->command_line[end] == ' ')
+                end++;
+            start = end;
         }
         end++;
     }
