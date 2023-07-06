2d5a4512cdd5d3382c197dc1bb40b779a1388a41
Laura <eccentricporcupine@gmail.com>
2023-07-06 10:12:03 +0200

    state

--- include/lexical_analyzer.h
+++ include/lexical_analyzer.h
@@ -7,6 +7,22 @@
 
 #include "minishell.h"
 
+typedef enum SM_STATES
+{
+    S_WHITESPACE = ' ',
+    S_METACHAR,
+    S_CHAR,
+    S_END,
+}SM_STATES;
+
+typedef struct s_state_machine
+{
+    SM_STATES state;
+    char *tolken;
+    SM_STATES next_state;
+}t_state_machine;
+
+
 void break_cmd(t_data *mini_data);
 
 #endif //MINISHELL_LEXICAL_ANALYZER_H
--- src/lexical_analyser/lexe.c
+++ src/lexical_analyser/lexe.c
@@ -1,6 +1,8 @@
 #include "../../include/minishell.h"
 #include "../../include/tolken_list_actions.h"
 #include "../../Lib42/include/libft.h"
+#include "../../include/lexical_analyzer.h"
+
 void if_pipe(t_data *mini_data, int start, int end)
 {
     if (mini_data->command_line[end+1] != ' ')
@@ -52,4 +54,37 @@ void break_cmd(t_data *mini_data)
     str = ft_substr(mini_data->command_line, start, end-start);
     add_token(&mini_data->tokens_list, str);
     print_tokens(mini_data->tokens_list);
+}
+void parse_str(t_state_machine *parser, char *command)
+{
+    char *buffer;
+
+}
+
+void parse_machine(t_state_machine *parser, char *command)
+{
+    while (*command != '\0')
+    {
+        if (parser->state == S_WHITESPACE)
+        {
+            if (*command == ' ') parser->state = S_WHITESPACE;
+            if (ft_isalnum(*command) != 0) parser->state = S_CHAR;
+            if (ft_strchr("<>|", *command) != 0) parser->state = S_METACHAR;
+            if (*command == '\0') parser->state = S_END;
+
+        }
+        else if (parser->state == S_CHAR)
+        {w
+            if (*command ==)
+        }
+        else if (parser->state == S_END)
+        {
+
+        }
+        else if (parser->state == S_METACHAR)
+        {
+
+        }
+        command++;
+    }
 }
