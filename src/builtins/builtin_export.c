f3ef36f3ca19e2fa0107a35f388aae3e6679adc8
Dominique Maessen <dmaessen@f1r2s15.codam.nl>
2023-07-27 16:33:18 +0200

    working on exort

--- src/builtins/builtin_export.c
+++ src/builtins/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: dmaessen <dmaessen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 13:25:10 by dmaessen          #+#    #+#             */
-/*   Updated: 2023/07/27 13:26:19 by dmaessen         ###   ########.fr       */
+/*   Updated: 2023/07/27 16:25:46 by dmaessen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,23 @@
 	// access envp
 	// open with write rights and add a line for it
 */
-int	builtin_export(t_data *mini)
+int	builtin_export(t_data *mini, char *cmd)
 {
 	int end;
+    int i;
+	int len;
+    char **str;
     
-
-	end = 0;
 	if (*mini->mini_envp == NULL)
 		return (ft_exit(errno));
+    cmd = ft_substr(cmd, 8, ft_strlen(cmd)); // needs protection??
+    str = ft_split(cmd, "=");
+	if (str == NULL)
+		return (NULL); // needs protection?? if NULL??
+	end = 0;
 	while (!mini->mini_envp[end])
 		end++;
-	// or
+	len = ft_strlen(str)
 	
 	// all depends if we use an array or a linekd list here
 	// do i need to malloc the line ? PROBABLY YES
