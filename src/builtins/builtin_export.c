93747d79e5a732e1ed155c3ae3f45057d08b9d74
Dominique Maessen <dmaessen@f1r2s15.codam.nl>
2023-07-27 16:58:09 +0200

    worked on export

--- src/builtins/builtin_export.c
+++ src/builtins/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: dmaessen <dmaessen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 13:25:10 by dmaessen          #+#    #+#             */
-/*   Updated: 2023/07/27 16:25:46 by dmaessen         ###   ########.fr       */
+/*   Updated: 2023/07/27 16:54:36 by dmaessen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,23 +37,35 @@ int	builtin_export(t_data *mini, char *cmd)
 {
 	int end;
     int i;
-	int len;
+	// char *str;
     char **str;
+	int nb; // amount of args to export
     
 	if (*mini->mini_envp == NULL)
 		return (ft_exit(errno));
-    cmd = ft_substr(cmd, 8, ft_strlen(cmd)); // needs protection??
-    str = ft_split(cmd, "=");
+    cmd = ft_substr(cmd, 8, ft_strlen(cmd)); // needs protection?
+	// if (cmd == NULL)
+	// 	return (NULL); // needs protection?? if NULL??
+	str = ft_split(cmd, " ");
 	if (str == NULL)
-		return (NULL); // needs protection?? if NULL??
+		return (NULL);
+	nb = 0;
+	while (str[nb])
+		nb++;
 	end = 0;
 	while (!mini->mini_envp[end])
 		end++;
-	len = ft_strlen(str)
-	
-	// all depends if we use an array or a linekd list here
-	// do i need to malloc the line ? PROBABLY YES
-
+	mini->mini_envp[end + 1] = malloc((ft_strlen(cmd) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (cmd[i])
+	{
+		mini->mini_envp[end + 1][i] = cmd[i];
+		i++;
+	}
+	mini->mini_envp[end + 1][i] = '\0'; // this ain't needed probably right ?? 
 	// check if there is more than one arg to set here (could be ++)
+	ft_free(str); // as its double pointer OR DOES THIS GET FREE UPON CLOSING OF THE PROG ONLY??
     return (0);
 }
