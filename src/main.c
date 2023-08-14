294e0fec21190a7470bf9ace037025a8a3b06db7
Laura <lpraca-l@student.codam.nl>
2023-08-14 14:15:04 +0200

    hey

--- src/main.c
+++ src/main.c
@@ -71,8 +71,8 @@ int	main(int argc, char **argv, char **envp)
 	mini_data = ft_calloc(1, sizeof(t_data));
 	if (mini_data == NULL)
 		ft_error("Data malloc fail!\n");
-//	parse_array_envp(mini_data, envp);
-	parse_list_envp(mini_data, envp);
+	parse_array_envp(mini_data, envp);
+//	parse_list_envp(mini_data, envp);
 	set_signals();
 	if (argc != 1)
 		ft_error("EROOR!!\nWrong amount of args!\n");
