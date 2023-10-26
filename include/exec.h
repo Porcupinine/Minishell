14e8e5fce2738d5c6c78b31a295c2e170f4d7638
domi <dmaessen@student.codam.nl>
2023-10-26 10:05:24 +0200

    finished commenting my functions

--- include/exec.h
+++ include/exec.h
@@ -6,7 +6,7 @@
 /*   By: domi <domi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 14:43:48 by dmaessen          #+#    #+#             */
-/*   Updated: 2023/10/25 23:12:48 by domi             ###   ########.fr       */
+/*   Updated: 2023/10/26 10:05:05 by domi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -264,30 +264,149 @@ int		not_alphanum(char *str);
  */
 int		find_path(char **envp);
 
+/**
+ * checks if the given command is a builtin, if so it sends it to the right function
+ * else it checks if envp is not NULL and if there is a path to the command (else 
+ * throws an error); if there is a path to the command it will look up that path 
+ * and try to execve the command.
+ * @param envp environement list
+ * @param mini
+ * @param cmd splitted command line
+ */
 char	*split_args(char *cmd, char **envp, t_data *mini);
 
+/**
+ * first tries to open in/out files if any (and check if the returned -1, if so exits with 1),
+ * then dup2 the in/out file to STDIN_FILENO/STDOUT_FILENO, and then calls the split_args()
+ * funtion
+ * @param mini
+ */
 void	run_one_cmd(t_data *mini);
+/**
+ * first creates one fork, then ignore_signals(), cheks if the fork failed if not in the child
+ * process it unset_signals() and run_one_cmd(). once that done closes the pipes, frees the fd
+ * and return with the latest exit_code.
+ * @param mini
+ */
 int		exec_fork_onecmd(t_data *mini);
 
+/**
+ * checks if any outfiles, if not set the file number to 1/STDOUT_FILENO;
+ * if there are outfiles, checks the type if its append or overwrite and accordingly opens the
+ * outfile (creates it if not existant), and goes on until all given outfiles have been opened.
+ * only the last outfile remains open, the rest is closes.
+ * exit_code set to 0 of all went well.
+ * @param commands to store the file's file number
+ * @param mini
+ */
 void	output_re(t_commands *commands, t_data *mini);
 
+/**
+ * checks if any infiles, if not set the file number to 0/STDIN_FILENO;
+ * if there are infiles opens them, and goes on until all given infiles have been opened.
+ * only the last outfile remains open, the rest is closes.
+ * exit_code set to 0 of all went well.
+ * @param commands to store the file's file number
+ * @param mini
+ */
 void	input_re(t_commands *commands, t_data *mini);
 
+/**
+ * prints out the given error message from str
+ * @param cmd command related to the error
+ * @param str error message
+ */
 void	err_msg(char *cmd, char *str);
+/**
+ * prints out the given error message from str, and sets the exit code to 0
+ * @param mini
+ * @param str error message
+ */
 void	error_msg(char *str, t_data *mini);
+/**
+ * checks the str for quotes and dollar sign to see if something needs to be expanded,
+ * if not it returns the strdup of the str.
+ * @param mini_data
+ * @param str string to check if vars to be expanded or not
+ */
 char	*expanded(char *str, t_data *mini_data);
+/**
+ * locates the dollar sign, and returns the the expanded line
+ * @param mini
+ * @param line string to be expanded
+ */
 char	*expand_dollar(char *line, t_data *mini);
-void	expansions(char *commands);
-int		expand_var(char **line, t_data *mini, int start);
+/**
+ * checks the lenght of the expansion, 
+ * @param start starting point of the expansion
+ * @param line string to be expanded
+ */
 int		var_len(char *line, int start);
+/**
+ * isolates the variable name and adds an equal sign after it, and returns the name + equal sign
+ * @param start starting point of the expansion name
+ * @param end starting point of the expansion name
+ * @param line string to be expanded
+ */
 char	*var_name(char *line, int start, int end);
+/**
+ * loops through the environement looking for var, and then returns what string that the var
+ * should expand to (without the name and equal sign in it)
+ * if not found returns an empty string
+ * @param var 
+ * @param mini
+ */
 char	*search_envp(char *var, t_data *mini);
+/**
+ * isolates the var to be expanded, then removed the var name and replaces it with the expansion,
+ * and returns the new expanded string
+ * @param line complete string to be expanded 
+ * @param var_exp the expansion of the variable 
+ * @param start start of the var name
+ * @param end end of the var name
+ */
 char	*var_replace(char *line, char *var_exp, int start, int end);
+/**
+ * isolates the var to be expanded, then removed the var name and replaces it with the expansion,
+ * and returns the new expanded string
+ * @param line complete string to be expanded 
+ * @param mini
+ * @param start start of the var name
+ */
 char	*char_expand_var(char *line, t_data *mini, int start);
 
+/**
+ * opens as many pipe(f_fd) as there are commands -1, sends to forkfork() for dup2's
+ * then connects the fds accordingly to set the read_end correctly for the next command
+ * and for the last command exec_last_cmd()
+ * and then pid_exit_code in order to exit and return with the right exit_code
+ * @param mini
+ */
 int		exec_fork(t_data *mini);
+/**
+ * waitpid until all the child process have finished, and then sets the right exist code
+ * also checking if there was a signal that interfeered or not
+ * @param mini
+ * @param pid to check on child processes completion
+ */
 void	pid_exit_code(t_data *mini, pid_t pid);
+/**
+ * creates a fork, ignore_signals and add the PID number to the end our our pid list
+ * then in the child process unsets the signals and sends the command to childchild();
+ * @param mini
+ * @param commands
+ * @param read_end
+ * @param p_fd
+ */
 int		forkfork(t_data *mini, t_commands *commands, int read_end, int *p_fd);
+/**
+ * closes p_fd[0], and dup3 the read_end to the STDIN_FILENO and p_fd[1] to STDOUT_FILENO
+ * then sends the command to split_args() and closes p_fd[1]
+ * @param mini
+ * @param commands
+ * @param read_end
+ * @param p_fd
+ */
 void	childchild(t_data *mini, t_commands *commands, int read_end, int *p_fd);
 
 #endif
